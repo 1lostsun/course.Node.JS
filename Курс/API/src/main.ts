e7b3de690269cc29bf85101a78964970159c3cf3
1lostsun <149478863+1lostsun@users.noreply.github.com>
2024-06-19 16:35:44 +0300

    12

--- Курс/API/src/main.ts
+++ Курс/API/src/main.ts
@@ -6,6 +6,9 @@ import { UserController } from './users/users.controller';
 import { ILogger } from './logger/logger.interface';
 import { TYPES } from './types';
 import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { IUserController } from './users/users.controller.interface';
+import { IUserService } from './users/users.interface';
+import { UserServise } from './users/users.service';
 
 export interface IBootstrapReturn {
 	appContainer: Container;
@@ -15,7 +18,8 @@ export interface IBootstrapReturn {
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<ILogger>(TYPES.ILogger).to(LoggerService);
 	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
-	bind<UserController>(TYPES.UserController).to(UserController);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUserService>(TYPES.UserServise).to(UserServise);
 	bind<App>(TYPES.Application).to(App);
 });
 
--- Курс/API/src/types.ts
+++ Курс/API/src/types.ts
@@ -2,5 +2,6 @@ export const TYPES = {
 	Application: Symbol.for('Application'),
 	ILogger: Symbol.for('LoggerService'),
 	UserController: Symbol.for('UserController'),
+	UserServise: Symbol.for('UserService'),
 	ExeptionFilter: Symbol.for('ExeptionFilter'),
 };
